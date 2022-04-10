00f66c07200289f0cea6bc6ff9042622b24dda42
Fathur Rohman <vatoer.ckplus@gmail.com>
2022-04-10 13:18:45 +0100

    step 2

--- urunan/ContentView.swift
+++ urunan/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     @State var tagihan: Double = 0.0
     @State var peserta: Int = 2
     @State var persenTip = 20
+    let persenTipTersedia = [0,5,10,15,20,25]
     var body: some View {
         Text("Urunan")
     }
