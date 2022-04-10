9e90cc31fcbc47a3e006938d4e178bda27a37cb2
Fathur Rohman <vatoer.ckplus@gmail.com>
2022-04-10 13:13:33 +0100

    mulai proyek urununan

---
+++ urunan/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  urunan
+//
+//  Created by fathur rohman on 09/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var tagihan: Double = 0.0
+    @State var peserta: Int = 2
+    @State var persenTip = 20
+    var body: some View {
+        Text("Urunan")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ urunan/urunanApp.swift
@@ -0,0 +1,17 @@
+//
+//  urunanApp.swift
+//  urunan
+//
+//  Created by fathur rohman on 09/04/22.
+//
+
+import SwiftUI
+
+@main
+struct urunanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
